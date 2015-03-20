0e439b4175bc543372a0a45a59cf92042d401858
Hoang Ngo <hoangngo.dev@gmail.com>
2015-03-20 04:44:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Find Different Color
+A simple game in which players find the cell with different color in a grid.
+
+## Screenshot
---
+++ main.js
@@ -0,0 +1,98 @@
+var GAMEPLAY_WIDTH = 10,
+    GAMEPLAY_HEIGHT = 10,
+    COLOR_DIFFERENCE = 10;
+
+function getRGB(red, green, blue) {
+  return 'rgb(' + [red, green, blue].join(',') + ')';
+}
+
+function removeAllChildren(elementId) {
+  while (true) {
+    var element = document.getElementById(elementId);
+    if (!element || !element.firstChild) break;
+    element.removeChild(element.firstChild);
+  }
+}
+
+function setupGrid() {
+
+  function createElementWithParentId(parentId, elementType, className) {
+    var parent = document.getElementById(parentId),
+        child  = document.createElement(elementType);
+
+    child.className = className;
+    parent.appendChild(child);
+  }
+
+  function createElementWithParentClass(parentClass, elementType, className) {
+    var parents = document.getElementsByClassName(parentClass);
+
+    for (var i = 0; i < parents.length; i++) {
+      var child = document.createElement(elementType);
+      child.className = className;
+      parents[i].appendChild(child);
+    }
+  }
+
+  removeAllChildren("gameplay");
+
+  // Adds rows
+  for (var i = 0; i < GAMEPLAY_HEIGHT; i++) {
+    createElementWithParentId("gameplay", "div", "row");
+  }
+
+  // Adds cells
+  for (var i = 0; i < GAMEPLAY_WIDTH; i++) {
+    createElementWithParentClass("row", "div", "cell");
+  }
+}
+
+function setupColor() {
+  var randomRed   = Math.floor(Math.random() * 256),
+      randomGreen = Math.floor(Math.random() * 256),
+      randomBlue  = Math.floor(Math.random() * 256),
+      rgb = [randomRed, randomGreen, randomBlue];
+  var randomColor = getRGB(rgb[0], rgb[1], rgb[2]);
+
+  // Sets background color for all cells
+  var cells = document.getElementsByClassName("cell");
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = randomColor;
+  }
+}
+
+function setupUniqueCell() {
+  var cells = document.getElementsByClassName("cell"),
+      index = Math.floor(Math.random() * cells.length),
+      color = cells[index].style.backgroundColor;
+
+  function parsePrimaryColor(rgb) {
+    var startIndex = rgb.indexOf('(') + 1,
+        endIndex   = rgb.indexOf(')');
+    var colorList  = rgb.substring(startIndex, endIndex);
+    return colorList.split(',');
+  }
+
+  // Creates new color for unique cell
+  var rgb   = parsePrimaryColor(color),
+      color = Math.floor(Math.random() * rgb.length);
+  rgb[color] += COLOR_DIFFERENCE;
+  randomColor = getRGB(rgb[0], rgb[1], rgb[2]);
+
+  cells[index].style.backgroundColor = randomColor;
+  cells[index].addEventListener("click", function() {
+    setupGameplay();
+  });
+}
+
+function setupGameplay() {
+  setupGrid();
+  setupColor();
+  setupUniqueCell();
+}
+
+function main() {
+  setupGameplay();
+}
+
+main();
